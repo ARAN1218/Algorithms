488cfccb23a9c1fa2aee9473ba58446faf7bacc9
ARAN <67265109+ARAN1218@users.noreply.github.com>
2023-01-21 00:57:48 +0900

    Update counting_sort.py

--- Sort/counting_sort.py
+++ Sort/counting_sort.py
@@ -1,11 +1,11 @@
-def counting_sort(data, max_value):
-    m = max_value + 1 # データ内の最大値max_valueがなければ、このソートは使えない
+def counting_sort(data, left, right):
+    m = right - left + 1 # データ内の最小値・最大値がなければ、このソートは使えない
     count = [0] * m # 度数分布のリストをゼロで初期化
     for a in data:
         print(a)
         count[a] += 1 # 出現回数の計算
     i = 0
-    for a in range(m): # 値の最大値まで繰り返す
+    for a in range(left, right): # 値の最大値まで繰り返す
         for c in range(count[a]): # 値の出現回数だけ繰り返して元のリストに値をコピーする
             data[i] = a
             i += 1
@@ -15,6 +15,7 @@ def counting_sort(data, max_value):
 # テスト
 import random
 data = [random.randint(0,9) for i in range(10)]
+min_value = min(data)
 max_value = max(data)
 print(data)
 print(counting_sort(data, max_value))
