932930f6bc540957bc23c3ca9458054ac1ec235c
ARAN <67265109+ARAN1218@users.noreply.github.com>
2023-02-04 19:18:05 +0900

    Update test.py

--- Local Search/test.py
+++ Local Search/test.py
@@ -37,3 +37,10 @@ data = [[((i[0]-j[0])**2 + (i[1]-j[1])**2)**0.5 for i in data] for j in data]
 tabu_search(data, currentSolution=[], steps=100, tabu_list_size=7)
 
 
+# genetic_algorithm テスト
+SIZE = 5
+MAP_SIZE = 100
+data = [[random.randint(0,MAP_SIZE) for i in range(2)] for j in range(SIZE)]
+data = [[((i[0]-j[0])**2 + (i[1]-j[1])**2)**0.5 for i in data] for j in data]
+
+genetic_algorithm(data, max_generation=10, solution_cnt=10)
