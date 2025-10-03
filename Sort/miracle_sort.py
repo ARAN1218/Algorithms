8ce2d7cb6a67d4d95be67fd93df8d49f9d49bc64
ARAN <67265109+ARAN1218@users.noreply.github.com>
2025-10-03 21:57:17 +0900

    Create miracle_sort.py

---
+++ Sort/miracle_sort.py
@@ -0,0 +1,57 @@
+def miracle_sort(data):
+  """
+  ミラクルソート（O(Faith^-1)）
+  データの並び替えが完了していない場合、神に祈る。
+
+  Args:
+    data (list): ソートする配列
+
+  Returns:
+    data_sorted (list): ソートされた配列
+  """
+
+  def pray():
+    """
+    神に祈る(Amazin grace)
+
+    Args:
+      your faith
+
+    Returns:
+      god bless. or...
+    """
+
+    """
+    Amazing grace! How sweet the sound!
+    That saved a wretch like me!
+    I once was lost, but now I am found;
+    Was blind, but now I see.
+
+    'Twas grace that taught my heart to fear,
+    And grace my fears relieved;
+    How precious did that grace appear!
+    The hour I first believed.
+
+    Through many dangers, toils, and snares,
+    I have already come;
+    'Tis grace hath brought me safe thus far,
+    And grace will lead me home.
+
+    Amazing grace! how sweet the sound
+    That saved a wretch like me
+    I once was lost, but now I am found
+    Was blind, but now I see.
+    """
+    return 
+
+  is_sorted = False
+  while not is_sorted:
+    pray()
+  
+  return data_sorted
+
+
+# テスト
+data = [5, 2, 4, 6, 1, 3]
+data_sorted = miracle_sort(data)
+print(data_sorted)
